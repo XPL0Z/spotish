f46383eb4413a76269c26a4854a4a1268a708fa8
XPL0Z <meneliksigrist2009@gmail.com>
2025-09-16 11:14:27 +0200

    test for autoupdate

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 from multiprocessing import Process
 import subprocess
+import git  # pip install gitpython
+import os 
 
+
+current_dir = os.getcwd() 
+
+repo = git.Repo(current_dir)
+
+current = repo.head.commit
+
+repo.remotes.origin.pull()
+
+if current != repo.head.commit:
+    print("It changed")
+else:
+    print("You have the last version")
+    
 scripts = ["bot.py", "music-player.py", "music-controller.py"]
+
 
 processes = []
 for s in scripts:
