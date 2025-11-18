f1aa80728eefe89b1a5b23cde789b75b11fdee2c
XPL0Z <meneliksigrist2009@gmail.com>
2025-11-18 13:56:13 +0100

    fix in the search bar,
    It didn't had access for the Clientid for Spotify

--- spotish-web/src/app/api/infos/route.jsx
+++ spotish-web/src/app/api/infos/route.jsx
@@ -3,15 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-        console.log("SECRETS :", process.env.HOST_CONTROLLER);
         let Api_url = process.env.HOST_CONTROLLER+process.env.CONTROLLER_PORT + "/infos";
-        console.log("Appel à l'API infos :", Api_url);
         // Ici, ton code backend (pause une action, enregistrer en DB, etc.)
         const res = await fetch(Api_url, {
         });
 
         const data = await res.json();
-        console.log("Données reçues :", data);
         return NextResponse.json(data);
     } catch (error) {
         console.error("Erreur dans la route GET :", error);
